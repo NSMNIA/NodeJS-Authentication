af089d06acccaa5b9f975a0443b1341a75ea17b0
Kevin Stoop <433243@student.fontys.nl>
2022-03-17 22:44:04 +0100

    added custom success message
    Signed-off-by: Kevin Stoop <433243@student.fontys.nl>

--- client/src/pages/Auth/Password/Change.tsx
+++ client/src/pages/Auth/Password/Change.tsx
@@ -10,6 +10,7 @@ const PasswordChange = (props: Props) => {
     const [currentPassword, setCurrentPassword] = useState<string>('');
     const [newPassword, setNewPassword] = useState<string>('');
     const [confirmPassword, setConfirmPassword] = useState<string>('');
+    const [changing, setChanging] = useState<boolean>(false);
     const [error, setError] = useState<string>('');
 
     const history = useNavigate();
@@ -21,6 +22,7 @@ const PasswordChange = (props: Props) => {
         if (newPassword !== confirmPassword) return setError('Please make sure your password match.');
         if (currentPassword === newPassword) return setError('Current password is equal to the new password.');
 
+        setChanging(true);
         axios.post(`${import.meta.env.VITE_APP_SERVER}/auth/password/change`, {
             currentPassword: currentPassword,
             newPassword: newPassword
@@ -31,9 +33,11 @@ const PasswordChange = (props: Props) => {
         }).then(res => {
             if (res.data.success === 0) return setError(res.data.message);
             Logging.info(res.data.message);
+            setChanging(false);
             return history('/profile');
         }).catch(err => {
             Logging.error(err);
+            setChanging(false);
             return setError('Something went wrong, try again later.');
         })
     }
@@ -45,7 +49,7 @@ const PasswordChange = (props: Props) => {
                 <input type="password" name="currentPassword" id="currentPassword" placeholder='Current password' onChange={e => setCurrentPassword(e.target.value)} />
                 <input type="password" name="newPassword" id="newPassword" placeholder='New password' onChange={e => setNewPassword(e.target.value)} />
                 <input type="password" name="confirmPassword" id="confirmPassword" placeholder='Confirm password' onChange={e => setConfirmPassword(e.target.value)} />
-                <button type="submit">Change password</button>
+                <button type="submit" disabled={changing}>Change password</button>
             </form>
             <ErrorText error={error} />
         </>
--- client/src/pages/Auth/Password/Forgot.tsx
+++ client/src/pages/Auth/Password/Forgot.tsx
@@ -8,12 +8,15 @@ type Props = {}
 const ForgotPassword = (props: Props) => {
     const [email, setEmail] = useState<string>('');
     const [error, setError] = useState<string>('');
+    const [updating, setUpdating] = useState<boolean>(false);
+    const [success, setSuccess] = useState<string>('');
 
     const resetEmail = (e: React.SyntheticEvent) => {
         e.preventDefault();
         if (error !== '') setError('');
         if (email.length === 0) return setError('Email is required.');
 
+        setUpdating(true);
         axios.post(`${import.meta.env.VITE_APP_SERVER}/auth/password/forgot`, {
             email: email
         }, {
@@ -23,9 +26,13 @@ const ForgotPassword = (props: Props) => {
         }).then(res => {
             if (res.data.success === 0) return setError(res.data.message);
             Logging.info(res.data.message);
+            setUpdating(false);
+            setSuccess(`Password reset link is sent to '${email}'`);
+            setEmail('');
 
         }).catch(err => {
             Logging.error(err);
+            setUpdating(false);
             return setError('Something went wrong, try again later.');
         })
     }
@@ -35,8 +42,11 @@ const ForgotPassword = (props: Props) => {
             <h1>Reset password</h1>
             <form action="" method='post' onSubmit={resetEmail}>
                 <input type="email" id='email' name='email' onChange={e => setEmail(e.target.value)} />
-                <button type="submit">Send password reset link</button>
+                <button type="submit" disabled={updating || success !== ''}>Send password reset link</button>
             </form>
+            {success !== '' && (
+                <>{success}</>
+            )}
             <ErrorText error={error} />
         </>
     )
